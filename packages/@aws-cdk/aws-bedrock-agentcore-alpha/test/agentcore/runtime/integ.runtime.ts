bc5fc8b292f06efbc5c87ed0b3eeda1b99af9c5e
Ahmed Hamed <aemada@amazon.com>
2026-02-03 14:59:22 +0100

    fix(bedrock-agentcore-alpha): fix integration tests
    - integ.runtime.ts: Delete snapshot folder and redeploy
    - integ.browser.ts: Delete snapshot folder and deploy in us-west-2
    - integ.target.ts: Add IAM policy dependencies to fix creation order

--- packages/@aws-cdk/aws-bedrock-agentcore-alpha/test/agentcore/runtime/integ.runtime.ts
+++ packages/@aws-cdk/aws-bedrock-agentcore-alpha/test/agentcore/runtime/integ.runtime.ts
@@ -107,5 +107,5 @@ new cdk.CfnOutput(stack, 'V1Endpoint2Id', {
 
 new integ.IntegTest(app, 'BedrockAgentCoreRuntimeTest', {
   testCases: [stack],
-  regions: ['us-west-2', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2'], // Bedrock Agent Core is only available in these regions
+  regions: ['us-east-1', 'us-east-2', 'us-west-2', 'ca-central-1', 'eu-central-1', 'eu-north-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'ap-northeast-1', 'ap-northeast-2', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2'], // Bedrock Agent Core is only available in these regions
 });
